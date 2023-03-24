fc1ac8cf9d0f6a6f06d013982b03882cc903b34c
Ivan Kosov <kosov.vanya@gmail.com>
2023-03-23 20:06:21 -0500

    Animals run from all four sides

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,6 +1,4 @@
 using Assets.Scripts;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -16,12 +14,37 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnRandomAnimal()
     {
-        var spawnX = Random.Range(Settings.Field.Left, Settings.Field.Right);
-        var spawnPos = new Vector3(spawnX, 0, Settings.World.Top);
+        var side = Random.Range(0, 4);
+
+        var field = Settings.Field;
+        var world = Settings.World;
+
+        var rotatation = 0f;
+        var spawnPos = new Vector3();
+
+        switch (side)
+        {
+            case 0: // top
+            case 1: // bottom
+                spawnPos.x = Random.Range(field.Left, field.Right);
+                spawnPos.z = side == 0 ? world.Top : world.Bottom;
+                rotatation = side == 0 ? 0 : 180;
+                break;
+            case 2: // right
+            case 3: // left
+                spawnPos.z = Random.Range(field.Top, field.Bottom);
+                spawnPos.x = side == 2 ? world.Right : world.Left;
+                rotatation = side == 2 ? 90 : 270;
+                break;
+        }
+
+        //var spawnX = Random.Range(field.Left, field.Right);
+        //var spawnPos = new Vector3(spawnX, 0, Settings.World.Top);
 
         var index = Random.Range(0, AnimalsPrefabs.Length);
         var animal = AnimalsPrefabs[index];
 
-        Instantiate(animal, spawnPos, animal.transform.rotation);
+        var spawnedAnimal = Instantiate(animal, spawnPos, animal.transform.rotation);
+        spawnedAnimal.transform.Rotate(Vector3.up, rotatation);
     }
 }
