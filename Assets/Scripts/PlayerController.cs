72e69d8df0a6170756c24f11f11b91d68fafb40e
Ivan Kosov <kosov.vanya@gmail.com>
2023-04-04 19:21:43 -0500

    #2 New controller controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,27 @@
 using Assets.Scripts;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+
+internal interface IControls
+{
+    bool IsFirePressed();
+    Vector3 ControlsPosition(float y);
+}
+
+internal class ControllerControls : IControls
+{
+    public Vector3 ControlsPosition(float y)
+    {
+        var verticalInput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+
+        return new Vector3(horizontalInput, y, verticalInput);
+    }
+
+    public bool IsFirePressed()
+    {
+        return Input.GetKeyDown(KeyCode.Joystick1Button0);
+    }
+}
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,22 +31,22 @@ public class PlayerController : MonoBehaviour
 
     public float ProjectileShift;
 
+    private IControls _controls = new ControllerControls();
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+        if (_controls.IsFirePressed())
         {
             var shift = transform.forward * ProjectileShift;
             Instantiate(FoodPrefab, transform.position + shift, transform.rotation);
         }
 
-        var verticalInput = Input.GetAxis("Vertical");
-        var horizontalInput = Input.GetAxis("Horizontal");
+        var controlsPosition = _controls.ControlsPosition(transform.position.y);
 
-        var controllerPosition = new Vector3(horizontalInput, transform.position.y, verticalInput);
-        var lookAt = controllerPosition + transform.position;
+        var lookAt = controlsPosition + transform.position;
         transform.LookAt(lookAt);
 
-        var pos = transform.position + controllerPosition * Speed * Time.deltaTime;
+        var pos = transform.position + controlsPosition * Speed * Time.deltaTime;
         pos.x = Mathf.Max(Settings.Field.Left, pos.x);
         pos.x = Mathf.Min(Settings.Field.Right, pos.x);
         pos.z = Mathf.Max(Settings.Field.Bottom, pos.z);
