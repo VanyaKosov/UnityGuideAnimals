0f98e9d817561f24d0462bc591efb68aca5da9b8
Ivan Kosov <kosov.vanya@gmail.com>
2023-04-15 20:06:08 -0500

    #7 Borders adapting to different screen ratio

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,4 +1,6 @@
-﻿namespace Assets.Scripts
+﻿using UnityEngine;
+
+namespace Assets.Scripts
 {
     internal struct Border
     {
@@ -8,9 +10,21 @@
         internal float Bottom;
     }
 
-    internal class Settings
+    internal class Settings : MonoBehaviour
     {
-        internal static Border Field = new Border{Left = -20, Right = 20, Top = 16, Bottom = -1 };
-        internal static Border World = new Border {Left = -23, Right = 23, Top = 19, Bottom = -4};
+        internal static Border Field = new Border { Left = -20, Right = 20, Top = 16, Bottom = -1 };
+        internal static Border World = new Border { Left = -23, Right = 23, Top = 19, Bottom = -4 };
+
+        private void Update()
+        {
+            var pixelsInMeter = Screen.height / (Field.Top - Field.Bottom);
+            var borderOffset = Screen.width / pixelsInMeter / 2;
+
+            Field.Right = borderOffset;
+            Field.Left = -borderOffset;
+
+            World.Right = Field.Right + 3;
+            World.Left = Field.Left - 3;
+        }
     }
 }
